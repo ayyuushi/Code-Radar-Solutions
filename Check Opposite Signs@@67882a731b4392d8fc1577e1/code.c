9548a9e0a1fa5a7b8a2fdac71f94fdd8d7958603
AYUSHI ANSHU <ayushianshu0401@gmail.com>
2025-02-24 12:59:59 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -6,10 +6,7 @@ int main(){
     if((a>0 && b>0) || (a<0 && b<0)){
         printf("False");
     }
-    else if(a==0 || b==0){
-        printf("Invalid");
-    }
-    else{
+    else if(!((a>0 && b>0) || (a<0 && b<0))){
         printf("True");
     }
     return 0;
